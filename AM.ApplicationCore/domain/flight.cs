629313d0fa9f5c94c6bc31fcf3dd7bbf1f500830
Nourelhouda <nour.farhat@esprit.tn>
2025-02-02 11:44:16 +0100

    first commit

---
+++ AM.ApplicationCore/domain/flight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.domain
+{
+    public class Flight
+    {
+        public String Destination { get; set; }
+        public String Departure { get; set; }
+       public DateTime FlightDate { get; set; }
+        public int FlightId { get; set; }
+        public String EffectiveArrival { get; set; }
+        public DateTime EffectiveArrivalDate { get; private set; }
+
+        public int EstimationDuration { get; set; }
+        public Plane plane { get; set; }
+        public ICollection<Passenger> passengers { get; set; }
+        
+    }
+}
---
+++ AM.UI.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AM.ApplicationCore.domain;
+
+Console.WriteLine("Hello, World!");
+
+Plane plane = new Plane();
+plane.Capacity  = 100;
+plane.ManufactureDate = DateTime.Now;
+plane.PlaneType = PlaneType.Airbus;
+
+//initialiseur dobjet
+
+Plane plane2 = new Plane
+{
+    Capacity = 100,
+    ManufactureDate = new DateTime(2025, 01, 30)
+    , PlaneType = PlaneType.Boeing
+};
+Console.WriteLine(plane);
+Passenger p1 = new Passenger { FirstName = "first", LastName = "last", EmailAddress = "first@gmail.com" };
+Staff staff = new Staff();
+Traveller traveller = new Traveller();
+
+p1.PassengerType();
+staff.PassengerType();
+traveller.PassengerType();
+Console.WriteLine("*****check profile*****");
+//Console.WriteLine(p1.CheckProfile("first","last"));
+Console.WriteLine(p1.CheckProfile("first","last","e"));
